1144222e4aa5fe7a89caf887dfeace5cafb90c05
2010030024-VEGESH <saivegeshbopp@gmail.com>
2023-11-25 19:23:11 +0530

    importing modules

--- model.py
+++ model.py
@@ -1,1 +1,20 @@
 #importing the modules
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+from tqdm.notebook import tqdm
+
+from tensorflow import keras
+from tensorflow.keras.preprocessing.image import load_img , array_to_img
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+
+warnings.filterwarnings('ignore')
+
+
+
+
+
